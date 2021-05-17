843f096cefc9f695655aca62e438feb98be785e8
maitrayee01 <79514984+maitrayee01@users.noreply.github.com>
2021-05-17 17:38:34 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -23,8 +23,19 @@ function addRoom()
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";
 snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
      Room_names = childKey;
-    //Start code
+    console.log("Room Name - " + Room_names);
+               row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>"; 
+               document.getElementById("output").innerHTML += row; 
+              }); 
+              }); 
+           }
 
-    //End code
-    });});}
+
 getData();
+
+function redirectToRoomName(name)
+{
+       console.log(name);
+       localStorage.setItem("room_name", name);
+       window.location = "kwitter_page.html";
+}
